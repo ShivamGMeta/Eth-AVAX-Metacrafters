c9a8271802081973e776bf7f009acb352810f6f5
ShivamG <149230344+ShivamGMeta@users.noreply.github.com>
2024-06-21 19:16:41 +0530

    Update errorFunction.sol

--- errorFunction.sol
+++ errorFunction.sol
@@ -1,29 +1,62 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
-contract NumberChecker {
-    function requireStatement(uint256 number) public pure  {
-        //verify inputs before execution
-        require(number > 0, "Number must be greater than zero");
-        // return "Executed Succefully";
+contract Voting {
+    struct Proposal {
+        string name;
+        uint voteCount;
     }
-    
-    function revertStatement(uint256 number) public pure returns (string memory){
-    //    changes back to intital stage 
-    // save the left amount of gas from getting lost
-        if (number % 2 == 0) {
-            return "NumberType => Even";
-        } else {
-            revert("Number is odd");
+
+    address public chairperson;
+    mapping(address => bool) public voters;
+    Proposal[] public proposals;
+
+    constructor(string[] memory proposalNames) {
+        chairperson = msg.sender;
+        for (uint i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
         }
     }
-    
-    function assertStatement(uint256 number) public pure returns (string memory){
-        assert(number < 100 && number >= 0);
-        if (number != 57){
-            return "Better Luck next time";
+
+    modifier onlyChairperson() {
+        require(msg.sender == chairperson, "Only chairperson can call this function.");
+        _;
+    }
+
+    function registerVoter(address voter) public onlyChairperson {
+        require(!voters[voter], "Voter is already registered.");
+        voters[voter] = true;
+    }
+
+    function vote(uint proposal) public {
+        require(voters[msg.sender], "You are not a registered voter.");
+        require(proposal < proposals.length, "Invalid proposal.");
+        proposals[proposal].voteCount += 1;
+    }
+
+    function winningProposal() public view returns (uint winningProposal_) {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
         }
-        return "You have won the lottery";
-        // To avoid conditions that should never be possible
+        assert(winningVoteCount > 0); // There should be at least one vote
+    }
+
+    function endVoting() public onlyChairperson {
+        if (proposals.length == 0) {
+            revert("No proposals found.");
+        }
+
+        for (uint i = 0; i < proposals.length; i++) {
+            delete proposals[i];
+        }
+
+        delete proposals;
     }
 }
